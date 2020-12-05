d294e38bb1339d696722906adfe1d0f1a39b2969
DMVance <csljrrt@gmail.com>
2020-12-04 23:48:09 -0600

    refactored app.py

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
 from flask import Flask, jsonify, render_template
-import csv, os
-import json
-import plotly
-import plotly.express as px
-# import pandas as pd
+from flask_cors import CORS
+import os, json
 
 app = Flask(__name__)
+CORS(app)
 
-def load_data():
-    with open("samples.json") as file:
-        data = json.load(file)
-    return data
-
-def create_plot_express():
-    data = load_data()
-    fig = px.bar(
-        x=[d["sample_values"] for d in data],
-        y=[d["otu_ids"] for d in data]
-    )
-
-    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # serialize the data for plot
-    # data = json.dumps(plot_data, cls=plotly.utils.PlotlyJSONEncoder)
-    # layout = json.dumps(plot_layout, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # return data, layout
+console.log("Good morning, World!")
 
 @app.route("/")
 def home():
-    fig = create_plot_express()
-    return render_template("index.html", fig=fig)
+    return render_template("index.html")
 
-# @app.route("/data")
-# def data():
+@app.route("/data")
+def data():
+    with open(os.path.join("samples.json")) as file:
+        data = json.load(file)
+    return data
 
+console.log("Good night, World!")
 
 if __name__ == "__main__":
     app.run(debug=True)
